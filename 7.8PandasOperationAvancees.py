ec83c6f70c8fdc8f028c69a9c99ebac2527c9cdb
julienPalleau <jpalleau@gmail.com>
2023-10-18 20:15:17 +0200

    file create

---
+++ 7.8PandasOperationAvancees.py
@@ -0,0 +1,5 @@
+"""
+Une des grandes forces de pandas est le support d'operations avancees, comme la concatenation, le merge qu'on peut
+egalement appeler jointure, le regroupement et le pivot. Ce sont des operations que l'on trouve habituellement dans
+les bases de donnees et qui sont extremement puissantes.
+"""
